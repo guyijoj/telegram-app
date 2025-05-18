539163d073b17018a3c715792b026c278132d1cd
Guyijoj <nguen.vin2005@mail.ru>
2025-05-18 21:37:39 +0300

    поправка

---
+++ src/components/EmploymentSection/VacancyInfo.tsx
@@ -0,0 +1,12 @@
+import { Forms } from "./Context/Context";
+
+interface VacanciesProps {
+  data: Forms;
+}
+export const VacancyInfo = () => {
+  return (
+    <>
+      <div className="text-3xl text-white">HEllO</div>
+    </>
+  );
+};
